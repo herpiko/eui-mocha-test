29ebc576d1328e3f39ed3d04faac012d2d4d1f3a
Herpiko Dwi Aguno <herpiko@gmail.com>
2019-09-10 21:45:27 +0700

    Implement an eui component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { EuiButton } from '@elastic/eui';
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <EuiButton>
+            Learn React
+          </EuiButton>
         </a>
       </header>
     </div>
